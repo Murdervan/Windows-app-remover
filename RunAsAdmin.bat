486b077aa40b4d9b9ee57aba6ee05ea9ad8846a8
Murdervan <123311683+Murdervan@users.noreply.github.com>
2026-03-18 20:34:52 +0100

    Delete RunAsAdmin.bat

--- RunAsAdmin.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set SCRIPT=%~dp0Windows-App-Remover.ps1
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^
- "Start-Process PowerShell -Verb RunAs -ArgumentList '-NoExit -ExecutionPolicy Bypass -File ""%SCRIPT%""'"
-
-exit
